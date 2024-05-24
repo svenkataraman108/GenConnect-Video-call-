3ef171c8bd78e443e969a4468d3c8e07969a2a95
@svenkataraman108 <kpvenkat108@gmail.com>
2024-05-24 16:03:16 -0500

    Initial Commit

---
+++ camera-access/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  camera-access
+//
+//  Created by Scholar on 5/24/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ camera-access/camera_accessApp.swift
@@ -0,0 +1,17 @@
+//
+//  camera_accessApp.swift
+//  camera-access
+//
+//  Created by Scholar on 5/24/24.
+//
+
+import SwiftUI
+
+@main
+struct camera_accessApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
